f3732182e41941cc8443a1787e5be08a9dce954a
Bryan Finlayson <bdfinlayson@gmail.com>
2016-05-09 23:16:24 -0500

    Add uniqueness validations

--- app/models/core_value.rb
+++ app/models/core_value.rb
@@ -1,5 +1,6 @@
 class CoreValue < ActiveRecord::Base
   validates_presence_of :name
+  validates_uniqueness_of :name
   has_many :acts, through: :qualities
   has_many :qualities
 end
--- app/models/quality.rb
+++ app/models/quality.rb
@@ -1,5 +1,6 @@
 class Quality < ActiveRecord::Base
   validates_presence_of :core_value_id, :assertion, :question
+  validates_uniqueness_of :assertion, :question
   belongs_to :core_value
   has_many :act_qualities
   has_many :acts, through: :act_qualities
