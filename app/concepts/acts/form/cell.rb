8c5046c0c21892176072848254f25d5eb5dfeb97
Bryan Finlayson <bdfinlayson@gmail.com>
2016-05-09 22:29:12 -0500

    Add form specs

--- app/concepts/acts/form/cell.rb
+++ app/concepts/acts/form/cell.rb
@@ -3,9 +3,9 @@ module Acts::Form::Cell
     include ActionView::RecordIdentifier
     include ActionView::Helpers::FormHelper
 
-    def unselected_core_values
-      return CoreValue.all if core_values.empty?
-      CoreValue.where.not(core_values.try(:map) {|x| x.try(:id)})
+    def unselected_qualities
+      return Quality.all if qualities.empty?
+      Quality.where.not(id: qualities.try(:map) {|x| x.try(:id)})
     end
   end
 end
--- spec/concepts/acts/form/cell_spec.rb
+++ spec/concepts/acts/form/cell_spec.rb
@@ -1,6 +1,27 @@
 module Acts::Form::Cell
   describe Show do
-    Given(:model) { described_class.(Act.first).model }
-    Then { model == Act.first }
+    let(:cell) { described_class }
+    context '#model' do
+      Given(:model) { cell.(Act.first).model }
+      Then { model == Act.first }
+    end
+
+    context '#unselected_qualities' do
+      let(:model) { cell.(Act.first) }
+
+      before do
+        model.qualities.destroy_all
+      end
+
+      it 'is empty' do
+        expect(model.unselected_qualities).to match_array Quality.all.map {|c| c }
+      end
+
+      it 'has unselected qualities' do
+        model.qualities.push [Quality.first, Quality.last]
+        expect(model.unselected_qualities.exclude?([Quality.first, Quality.last])).to be true
+        expect(model.unselected_qualities.count).to eq Quality.count - 2
+      end
+    end
   end
 end
