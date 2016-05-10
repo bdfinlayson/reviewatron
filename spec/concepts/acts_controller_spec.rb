7ce45a84795df77632157d56b10e07eb8e175d30
Bryan Finlayson <bdfinlayson@gmail.com>
2016-05-09 20:57:42 -0500

    Revise tests

--- spec/concepts/acts_controller_spec.rb
+++ spec/concepts/acts_controller_spec.rb
@@ -1,14 +1,24 @@
 describe ActsController, type: :controller do
   context '#new' do
-    Given { sign_in User.first }
-    When { get :new }
-    Then { response.status == 200 }
+    it do
+      sign_in User.first
+      get :new
+      expect(response).to have_http_status :success
+    end
   end
 
   context '#create' do
-    Given { sign_in User.first }
-    When { post :create, act: { description: "blah" } }
-    Then { Act.last.description == 'blah' }
-    And { Act.last.user == User.first }
+    before do
+      sign_in User.first
+    end
+    it 'success' do
+      expect{post :create, act: { description: "blah" }}.to change(Act, :count).by(1)
+      expect(Act.last.description).to eq 'blah'
+      expect(Act.last.user).to eq User.first
+    end
+
+    it 'fail' do
+      expect{ post :create, act: { description: '' }}.to change(Act, :count).by(0)
+    end
   end
 end
