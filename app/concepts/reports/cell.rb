d4302c2fe353d659848d267d7f9022544fff8a37
Bryan Finlayson <bdfinlayson@gmail.com>
2016-06-24 18:29:10 -0500

    add cell methods, reduce view logic

--- app/concepts/reports/cell.rb
+++ app/concepts/reports/cell.rb
@@ -1,8 +1,17 @@
 module Reports::Cell
   class Index < Trailblazer::Cell
-    def judgement
-      CoreValue.find_by(name: 'Judgement').acts.where(user: model)
+    def core_values_order
+      %w(Judgement Communication Impact Curiosity Innovation Courage Passion Honesty Selflessness)
     end
-
+    def core_values_qualities_questions(cv_name)
+      questions = []
+      CoreValue.find_by(name: cv_name).qualities.each do |q|
+        questions.push q.question
+      end
+      questions
+    end
+    def core_value_acts(cv_name)
+      Act.includes(:core_values).where(user: model).where(core_values: {name: cv_name})
+    end
   end
 end
