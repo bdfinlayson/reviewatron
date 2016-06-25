f58bbf02887915ecc4c536dba8b0c410ea01162d
Bryan Finlayson <bdfinlayson@gmail.com>
2016-06-24 19:16:57 -0500

    Add report cell specs

--- spec/concepts/reports/cell_spec.rb
+++ spec/concepts/reports/cell_spec.rb
@@ -1,12 +1,23 @@
 module Reports::Cell
   describe Index do
-    context 'Judgement' do
-      before do
-        quality = CoreValue.find_by(name: 'Judgement').qualities.first
-        User.first.acts.push Act.create(description: 'blah', qualities: [quality] )
+    context '#core_values_order' do
+      it 'has the correct order of core values' do
+        expect(described_class.(User.first).core_values_order).to match_array %w(Judgement Communication Impact Curiosity Innovation Courage Passion Honesty Selflessness)
       end
-      it 'gets all the judgement acts' do
-        expect(described_class.(User.first).judgement.count).to eq 1
+    end
+    context '#core_values_qualities_questions' do
+      it 'returns an array of questions' do
+        expect(described_class.(User.first).core_values_qualities_questions('Communication')).to match_array ["Do you listen well?", "Do you impulsively react?", "Are you concise and articulate in speech and writing?", "Do you treat people with respect?", "Do you maintain calm and poise in stressful situations?"]
+      end
+    end
+    context '#core_value_acts' do
+      before do
+        User.first.acts.destroy_all
+        User.first.acts.push Act.create(description: 'blah', qualities: [CoreValue.find_by(name: 'Communication').qualities.first])
+        User.first.acts.push Act.create(description: 'wowza', qualities: [CoreValue.find_by(name: 'Communication').qualities.first])
+      end
+      it 'returns an array of acts' do
+        expect(described_class.(User.first).core_value_acts('Communication').count).to eq 2
       end
     end
   end
